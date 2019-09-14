94b96cb144aa9e6cd65053034dab27fbbafa2b63
Ariel Plomer <asplomer@gmail.com>
2019-09-13 22:15:55 -0300

    primeros script de Behaviour trees
    Behaviour trees: falta escribir todo

---
+++ RTS/RTS-TruchiEmpires/Assets/Scripts/NodeAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeAction : Nodo
+{
+    //running, ok, fail
+    private void Start()
+    {
+        tipoNodo = "Action";
+        state = NodeState.running;
+    }
+    public void Action()
+    {
+        if(state == NodeState.running)
+        {
+                   
+
+            
+        }
+       
+    }
+}

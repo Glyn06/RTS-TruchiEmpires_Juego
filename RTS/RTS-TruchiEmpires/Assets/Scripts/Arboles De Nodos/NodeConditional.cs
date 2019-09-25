b133233041d5f50d8b627902c77fc84de657520c
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2019-09-25 15:55:31 -0300

    carpeta del arbol de nodos

---
+++ RTS/RTS-TruchiEmpires/Assets/Scripts/Arboles De Nodos/NodeConditional.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeConditional : Nodo
+{
+    //if ok returns fail, else returns ok
+    public int Conditional()
+    {
+        if (state == NodeState.fail)
+        {
+            return 2;       //ok
+        }
+        return 3;           //fail
+    }
+}
---
+++ RTS/RTS-TruchiEmpires/Assets/Scripts/Arboles De Nodos/Nodo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Nodo : MonoBehaviour
+{
+    public enum NodeState
+    {
+        none,
+        running,
+        ok,
+        fail,
+        count
+    }
+    public NodeState state;
+    public string tipoNodo;
+}

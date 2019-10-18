5620f4feda2964cf7387e96473b5ea4eac222519
Ariel Plomer <asplomer@gmail.com>
2019-10-18 19:56:23 -0300

    Tremendo Flocking vieja

---
+++ RTS/Flocking with Boids/Assets/Scripts/AIBoid.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIBoid : MonoBehaviour
+{
+
+    public Vector3 myDirection; //para donde mira
+
+
+
+    void Start()
+    {
+        myDirection = transform.forward;
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ RTS/Flocking with Boids/Assets/Scripts/AIFlocking.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIFlocking : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public Vector3 Alineacion()
+    {
+        return Vector3.zero;
+    }
+    public Vector3 Cohesion()
+    {
+        return Vector3.zero;
+    }
+    public Vector3 Separacion()
+    {
+        return Vector3.zero;
+    }
+}

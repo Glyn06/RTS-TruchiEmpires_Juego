f3e0c30c62e7c1d4b97a6eab6150580864a54f1a
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2019-10-18 21:19:21 -0300

    NAAAAAA TREMENDO

--- RTS/Flocking with Boids/Assets/Scripts/AIBoid.cs
+++ RTS/Flocking with Boids/Assets/Scripts/AIBoid.cs
@@ -6,17 +6,40 @@ public class AIBoid : MonoBehaviour
 {
 
     public Vector3 myDirection; //para donde mira
-
-
+    public Vector3 FinalPosition;
+    public ObjectClicked objectClicked;
+    public float rootSpeed;
 
     void Start()
     {
+        if (ObjectClicked.instanceObjectClicked != null)
+        {
+            objectClicked = ObjectClicked.instanceObjectClicked;
+        }
         myDirection = transform.forward;
     }
 
 
     void Update()
     {
-        
+        CheckMyDireccion();
+        MovementLerp();
+        CheckFinalPosition();
+    }
+    public void CheckFinalPosition()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            FinalPosition = objectClicked.CheckHitMouse();// ponerle normalized si no lo normalizo en las 
+        }
+    }
+    public void MovementLerp()
+    {
+        transform.position += transform.forward/50;
+        transform.forward = Vector3.Slerp(transform.forward, FinalPosition, rootSpeed * Time.deltaTime);
+    }
+    public void CheckMyDireccion()
+    {
+        myDirection = transform.forward;
     }
 }

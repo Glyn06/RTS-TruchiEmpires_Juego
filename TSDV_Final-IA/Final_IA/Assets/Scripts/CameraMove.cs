20e16509448ad35cf49667a24a2aec6516d794b8
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2020-11-28 18:11:59 -0300

    update camera

--- TSDV_Final-IA/Final_IA/Assets/Scripts/CameraMove.cs
+++ TSDV_Final-IA/Final_IA/Assets/Scripts/CameraMove.cs
@@ -6,12 +6,17 @@ public class CameraMove : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform cameraAnchor;
+    public Transform cameraPivot;
 
     private int border;
     private Vector2 screenBorder;
     private Vector2 middleScreen;
 
     private Vector3 movement;
+
+    [SerializeField]private float speedHorizontalMovement = 2.0f;
+    [SerializeField]private float speedVerticalMovment = 2.0f;
+
     void Start()
     {
         border = 5;
@@ -22,23 +27,27 @@ public class CameraMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
-        //{
-            Vector2 mouse = Input.mousePosition;
-            if ((Input.mousePosition.x < border) ||
-                (Input.mousePosition.x > screenBorder.x) ||
-                (Input.mousePosition.y < this.border) ||
-                (Input.mousePosition.y > this.screenBorder.y))
-            {
-                movement = mouse - middleScreen;
-                movement.z = movement.y;
-                movement.y = 0;
-                movement = movement.normalized / 2f;
+        Vector2 mouse = Input.mousePosition;
+        if ((Input.mousePosition.x < border) ||
+            (Input.mousePosition.x > screenBorder.x) ||
+            (Input.mousePosition.y < this.border) ||
+            (Input.mousePosition.y > this.screenBorder.y))
+        {
+            movement = mouse - middleScreen;
+            movement.z = movement.y;
+            movement.y = 0;
+            movement = movement.normalized / 2f;
 
-                cameraAnchor.Translate(movement);
-            }
+            cameraAnchor.Translate(movement * speedHorizontalMovement);
+        }
 
-
-        //}
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            cameraPivot.transform.position = new Vector3(cameraPivot.transform.position.x, cameraPivot.transform.position.y - speedVerticalMovment, cameraPivot.transform.position.z);
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            cameraPivot.transform.position = new Vector3(cameraPivot.transform.position.x, cameraPivot.transform.position.y + speedVerticalMovment, cameraPivot.transform.position.z);
+        }
     }
 }

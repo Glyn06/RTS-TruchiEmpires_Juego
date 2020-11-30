a627ee092ac9264ff75ff17571ce6c565a57245c
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2020-11-30 14:55:22 -0300

    Redes neuronales y Adgoritmo Genetico

---
+++ TSDV_Final-IA/Final_IA/Assets/Redes Neuronales y Algoritmo Genetico/Scripts/Test/Tank.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tank : TankBase
+{
+
+    protected override void OnReset()
+    {
+
+    }
+
+    protected override void OnThink(float dt) 
+	{
+        Vector3 dirToMine = GetDirToMine(nearMine);
+
+        Vector3 dir = this.transform.forward;
+
+        inputs[0] = dirToMine.x;
+        inputs[1] = dirToMine.y;
+        inputs[2] = dir.x;
+        inputs[2] = dir.y;
+
+        float[] outputs = brain.Synapsis(inputs);
+
+        SetForces(outputs[0], outputs[1], dt);
+
+        //
+    }
+    
+    protected override void OnTakeMine(GameObject mine)
+    {
+        genome.fitness++;
+    }
+}
